ad2c4e29874767aa9550060c644c16aad760eca2
Parthu33 <102277718+Parthw33@users.noreply.github.com>
2024-05-04 15:14:59 +0530

    Add Mongoose & Router

---
+++ routes/User.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  login,
+  signup,
+} = require("../../../Sem 6/SE Lab/FoodLens/backend/controllers/auth");
+
+router.post("/login", login);
+
+router.post("/signup", signup);
+
+module.exports = router;
