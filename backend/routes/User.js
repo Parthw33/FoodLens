f6bdb2690229b758d570c6c1cc724425c712135f
Parthu33 <102277718+Parthw33@users.noreply.github.com>
2024-05-04 23:43:15 +0530

    Changing the model & user routes

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {
   login,
   signup,
-} = require("../../../Sem 6/SE Lab/FoodLens/backend/controllers/auth");
+} = require("../../../../Sem 6/SE Lab/FoodLens/backend/controllers/auth");
 
 router.post("/login", login);
 
